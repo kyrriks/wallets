e14f477148faf4c7ed2997cb762233a8db866ec9
user1 <user1@MacBook-Air-user1.local>
2025-03-10 21:27:30 +0300

    add docker

--- wallets/api/wallets/serializers.py
+++ wallets/api/wallets/serializers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 
 from wallets.models import Wallet
@@ -5,7 +6,7 @@ from wallets.enums import OperationType
 
 
 class WalletTransactionRequestSerializer(serializers.Serializer):
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=0)
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=Decimal())
     operation_type = serializers.ChoiceField(choices=OperationType)
 
 
