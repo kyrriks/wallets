5007f0f2fa94663f0c28f8c6150b0ea5f1c71140
user1 <user1@MacBook-Air-user1.local>
2025-03-10 19:31:02 +0300

    add atomic transaction for deposit or withdraw operations

--- wallets/api/wallets/services.py
+++ wallets/api/wallets/services.py
@@ -1,13 +1,17 @@
+from django.db import transaction
+
 from wallets.models import Wallet
 
 
 def deposit(wallet: Wallet, amount: float):
-    wallet.balance += amount
-    wallet.save()
+    with transaction.atomic():
+        wallet.balance += amount
+        wallet.save()
 
 
 def withdraw(wallet: Wallet, amount: float):
-    if wallet.balance - amount < 0:
-        raise ValueError('Недостаточно средств на счете')
-    wallet.balance -= amount
-    wallet.save()
+    with transaction.atomic():
+        if wallet.balance - amount < 0:
+            raise ValueError('Недостаточно средств на счете')
+        wallet.balance -= amount
+        wallet.save()
